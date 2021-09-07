985559698d2a3c230be12096023b0990cd18c149
Ematylickiy <79719652+Ematylickiy@users.noreply.github.com>
2021-09-07 22:27:41 +0300

    home work

--- src/actions/mountAuthorPage.js
+++ src/actions/mountAuthorPage.js
@@ -1,14 +1,21 @@
 import API from '../api';
 import AuthorPage from '../components/AuthorPage';
 
+
 async function mountAuthorPage(userId) {
   try {
     const data = await API.users.getById(userId);
     const AuthorPageComponent = new AuthorPage();
     const posts = await API.posts.getByUserId(userId);
+    const photos = await API.albums.getPhotosById();
+    const {title, url} = photos[0]
     const props = {
       ...data,
       posts,
+      photos: {
+        title,
+        url
+      }
     };
     AuthorPageComponent.initialize(props);
   } catch (error) {
--- src/actions/mountPostPage.js
+++ src/actions/mountPostPage.js
@@ -1,26 +1,34 @@
 import API from '../api';
 import PostPage from '../components/PostPage';
 
+
+let props = '';
+let currentId = ''
 async function mountPostPage(postId) {
-  try {
-    const { userId, ...rest } = await API.posts.getById(postId);
-    const PostPageComponent = new PostPage();
-    const { username, website, company } = await API.users.getById(userId);
-    const comments = await API.comments.getByPostId(postId);
-    const props = {
-      comments,
-      author: {
-        username,
-        id: userId,
-        website,
-        companyName: company.name,
-      },
-      ...rest,
-    };
-    PostPageComponent.initialize(props);
-  } catch (error) {
-    console.log(error);
+  const PostPageComponent = new PostPage();
+  if (!props || currentId != postId) {
+    currentId = postId
+    try {
+      const { userId, ...rest } = await API.posts.getById(postId);
+      const { username, website, company } = await API.users.getById(userId);
+      const comments = await API.comments.getByPostId(postId);
+      props = {
+        comments,
+        author: {
+          username,
+          id: userId,
+          website,
+          companyName: company.name,
+        },
+        ...rest,
+      };
+    } catch (error) {
+      console.log(error);
+    }
   }
+  
+  PostPageComponent.initialize(props);
+
 }
 
 export default mountPostPage;
